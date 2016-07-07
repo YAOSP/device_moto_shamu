62baa1a045a380772d1c8f38e712a57c28add419
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-07-08 01:16:13 +0200

    powerHAL: Remove userspace touchboost
    Stop flooding logs with touchboost "failed to send", we have in-kernel touchboost
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- power/power_shamu.c
+++ power/power_shamu.c
@@ -149,7 +149,7 @@ static void process_video_encode_hint(void *metadata)
     }
 }
 
-
+/*
 static void touch_boost()
 {
     int rc, fd;
@@ -171,7 +171,7 @@ static void touch_boost()
         //ALOGE("%s: failed to send: %s", __func__, strerror(errno));
     }
 }
-
+*/
 static void low_power(int on)
 {
     int rc;
@@ -225,7 +225,7 @@ static void power_set_interactive(__attribute__((unused)) struct power_module *m
     ALOGV("%s %s", __func__, (on ? "ON" : "OFF"));
     if (on) {
         coresonline(0);
-        touch_boost();
+//        touch_boost();
     } else {
         coresonline(1);
     }
@@ -271,7 +271,7 @@ static void power_hint( __attribute__((unused)) struct power_module *module,
     switch (hint) {
         case POWER_HINT_INTERACTION:
             //ALOGV("POWER_HINT_INTERACTION");
-            touch_boost();
+//            touch_boost();
             break;
 #if 0
         case POWER_HINT_VSYNC:
